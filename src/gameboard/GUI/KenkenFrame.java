5db2a1747cf27423695574203a3e9a0fb12a6a64
Jouse <josuecanalesmena@gmail.com>
2018-04-20 00:49:29 -0600

    jh

--- src/gameboard/GUI/KenkenFrame.java
+++ src/gameboard/GUI/KenkenFrame.java
@@ -1,12 +1,15 @@
 package gameboard.GUI;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
 import gameboard.logic.board.Generator;
 import gameboard.logic.board.KenkenBoard;
 
-import javax.sql.rowset.spi.XmlReader;
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class KenkenFrame extends JFrame {
@@ -15,6 +18,10 @@ public class KenkenFrame extends JFrame {
     private JButton generatorButton;
     private JComboBox<Object> comboBox;
     private JTextField tf;
+    private FileReader reader;
+    private final XStream xStream;
+    private PrintWriter out;
+    private String xml;
 
     public KenkenFrame(){
 
@@ -22,22 +29,10 @@ public class KenkenFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("KenKen");
         //this.setResizable(false);
-        this.setMinimumSize(new Dimension(850,750));
+        this.setMinimumSize(new Dimension(700,760));
+        xStream = new XStream(new DomDriver());
 
-        /*
-        JMenuBar menuBar = new JMenuBar();
-        JMenu file = new JMenu("File");
-        JMenuItem load = new JMenuItem("Open File");
-        JMenu newTable = new JMenu("new KenKen");
-        JMenuItem save = new JMenuItem("Save Table");
-
-        file.add(newTable);
-        file.add(load);
-        file.add(save);
-        menuBar.add(file);
-        this.setJMenuBar(menuBar);
-
-         */
+        kPanel = new KenkenPanel(new KenkenBoard(8,8));
 
         comboBox = new JComboBox<>();
         tf = new JTextField(20);
@@ -50,7 +45,7 @@ public class KenkenFrame extends JFrame {
 
         generatorButton = new JButton("New Table");
         generatorButton.addActionListener( e -> {try{
-            this.rebuildInterface((int) comboBox.getItemAt(comboBox.getSelectedIndex()));
+            this.rebuildInterface((int) comboBox.getItemAt(comboBox.getSelectedIndex()), true);
         }catch (Exception exc){
             exc.printStackTrace();
         }});
@@ -58,33 +53,51 @@ public class KenkenFrame extends JFrame {
         JButton saveTable = new JButton("Save Table");
         saveTable.addActionListener(e -> {
             try{
-                XStream xStream = new XStream();
+                FileDialog fd = new FileDialog(this, "Save Table", FileDialog.SAVE);
+                fd.setDirectory("\\Tables");
+                fd.setFile("Table.xml");
+                fd.setVisible(true);
+                String name = fd.getFile();
+                out = new PrintWriter(fd.getDirectory() + name);
+                ArrayList<String> data = new ArrayList<>();
+                xml = xStream.toXML(kPanel.getBoard());
+                data.add(xml);
+                out.println(xml);
+                out.close();
             }catch (Exception ignored){
 
             }
                 }
         );
+
         JButton openTable = new JButton("Load Table");
         openTable.addActionListener(e -> {
             try{
                 FileDialog fd = new FileDialog(this, "Choose a file", FileDialog.LOAD);
-                fd.setDirectory("..\\Tables");
+                fd.setDirectory(this.getClass().getProtectionDomain().getCodeSource().getLocation().toString()+"\\Tables");
                 fd.setFile("*.xml");
                 fd.setVisible(true);
                 String filename = fd.getFile();
                 if (filename == null)
                     System.out.println("You cancelled the choice");
-                else
-                    System.out.println("You chose " + filename);
-            }catch (Exception ignored){}
+                else{
+                    reader = new FileReader(fd.getDirectory()+filename);
+                    KenkenBoard board = (KenkenBoard) (xStream.fromXML(reader));
+                    kPanel.setBoard(board);
+                    rebuildInterface(board.getNumColums(), false);
+                }
+            }catch (Exception e1){
+                e1.printStackTrace();
+            }
         });
 
         JPanel windowPanel = new JPanel();
         windowPanel.setLayout(new FlowLayout());
 
-        kPanel = new KenkenPanel(new KenkenBoard(8,8));
+        JLabel lbl = new JLabel("ケンケン (KenKen)");
+        lbl.setFont(new Font(Font.SANS_SERIF,Font.BOLD,20));
 
-
+        windowPanel.add(lbl);
         windowPanel.add(kPanel);
         windowPanel.add(comboBox);
         windowPanel.add(generatorButton);
@@ -94,9 +107,11 @@ public class KenkenFrame extends JFrame {
 
     }
 
-    public void rebuildInterface(int dimension){
-        KenkenBoard generatedboard = new KenkenBoard(dimension,dimension);
-        kPanel.setBoard(generatedboard);
+    public void rebuildInterface(int dimension,boolean newTable){
+        if(newTable){
+            KenkenBoard generatedboard = new KenkenBoard(dimension,dimension);
+            kPanel.setBoard(generatedboard);
+        }
         int fontSize = Generator.calculateFontSize(dimension);
         kPanel.setFontSize(fontSize);
         kPanel.setGapY(fontSize);
